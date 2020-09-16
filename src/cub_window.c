f19d0eda2a9571cbd856b14754a5f4d77bf82f6f
Silk Herbert <sherbert@am-a3.msk.21-school.ru>
2020-09-16 18:30:43 +0300

    map

--- src/cub_window.c
+++ src/cub_window.c
@@ -1,6 +1,35 @@
 #include "cub3d.h"
 
-// static void    print_map(t_ident ident)
+static void    print_map(t_ident ident, void *mlx, void *win)
+{
+    int i;
+    int j;
+    int x_m;
+    int y_m;
+    int x = ident.x / 2 - ident.size * 5;
+    int y = ident.y / 2 - ident.len * 5;
+
+    i = -1;
+    while (++i < ident.size)
+    {
+        x = ident.x / 2 - ident.size * 15;
+        j = -1;
+        while (++j < ident.len)
+        {
+            if (ident.map[i][j] == 1)
+            {
+                for(x_m = 0; x_m < 15; x_m++)
+                {
+                    for(y_m = 0; y_m < 15; y_m++)
+                        mlx_pixel_put(mlx, win, x + x_m, y + y_m, 0xFF00EF);
+                }
+            }
+            x += 15;
+        }
+        y += 15;
+    }
+}
+
 int main(int argc, char **argv)
 {
     void    *mlx;
@@ -13,12 +42,7 @@ int main(int argc, char **argv)
     {
         mlx = mlx_init();
         win = mlx_new_window(mlx, parce.x, parce.y, "cub3d");
-        while (y++ < 150)
-        {
-            x = 100;
-            while (x++ < 200)
-                mlx_pixel_put(mlx, win, x, y, 0xFF00EF);
-        }
+        print_map(parce, mlx, win);
         mlx_loop(mlx);
     }
     return (0);
