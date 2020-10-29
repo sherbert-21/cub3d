0e7b2c79ebfa74bb2ba11b220fb2c96f08721682
dolovnyak <dolovnyak@yandex.ru>
2020-10-30 00:11:55 +0300

    added cmakelist

--- src/sprite.c
+++ src/sprite.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../../includes/cub3d.h"
+#include "cub3d.h"
 
 static void		next_sprite(
 	t_sprites *actual,
